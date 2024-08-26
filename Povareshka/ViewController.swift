76e39567c5aee8307543ce40a3af8d93a4911c4c
Pavel <lmr_16@mail.ru>
2024-08-26 12:14:30 +0300

    Initial Commit

---
+++ Povareshka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Povareshka
+//
+//  Created by Pavel Lakhno on 26.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
